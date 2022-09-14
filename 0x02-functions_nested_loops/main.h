d2b27b077669b6da3fc835aefa5c9183bf607152
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-15 01:27:34 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 #include<stdio.h>
 int _putchar(char);
 void print_alphabet(void);
@@ -14,4 +14,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-endif /* MAIN_H */
+#endif
