ea995ba21de824564f095deb30a9b88b995fbbd5
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-15 01:16:59 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,9 @@
 #include "main.h"
-#include <string.h>
 /**
-* main - Entry point* Description: prints _putchar using putchar prototype
+* main - Entry point
 *
-* Description: prints _putchar using putchar prototype
+* Return: Always returns 0
 *
-* Return: Always 0 (Success)
 */
 int main(void)
-char str[] = "_putchar";
-int ch;
-for (ch = 0; ch < 8; ++ch)
-_putchar(str[ch]);
-_putchar('\n');
-return (0)
-}
+{
