a990e8ba6b9cc0482b9cb6e919cc428ed7eac9a4
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-16 00:36:43 +0200

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
-/**
 
+/**
 * print_remaining_days - takes a date and prints how many days are
 * left in the year, including leap years
 * @month: month in number format
