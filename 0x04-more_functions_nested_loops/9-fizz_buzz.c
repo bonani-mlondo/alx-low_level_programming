3febdddb95c842261386b9a851815c1eaf73699d
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-17 04:52:40 +0200

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 int n;
-for (n = 1; n <= 100; n++)
+n = 1;
+printf("%d", n);
+for (n = 2; n <= 100; n++)
 {
 if (n % 3 == 0 || n % 5 == 0)
-{
-if (n % 3 == 0)
 {
 printf("FizzBuzz");
 }
@@ -21,11 +21,11 @@ printf("Fizz");
 }
 else if (n % 5 == 0)
 {
-printf("Buzz");
+printf(" Buzz");
 }
 else
 {
-printf("%d", n);
+printf(" %d", n);
 }
 }
 printf("\n");
