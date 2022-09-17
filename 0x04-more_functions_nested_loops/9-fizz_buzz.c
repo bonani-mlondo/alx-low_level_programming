7f139883b2443e3d9bac21fbb9112a4c7ce4af7f
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-17 04:42:54 +0200

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* main - main function
-* Return: a number
+* main - prints Buzz each numbers of 3 and 5.
+* Return: Always 0.
 */
 int main(void)
 {
-int i;
-for (i = 1; i <= 100; i++)
+int n;
+for (n = 1; n <= 100; n++)
 {
-if (i % 3 == 0 || i % 5 == 0)
+if (n % 3 == 0 || n % 5 == 0)
 {
-if (i % 3 == 0)
+if (n % 3 == 0)
 {
 printf("FizzBuzz");
 }
