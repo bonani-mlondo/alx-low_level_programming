f3bdbbfa8cebb68326c6d9ca2e4366f661257200
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-20 01:06:59 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,27 +1,27 @@
-#include "main.h"
+#include "main.h"
 /**
-* rev_string - reverses a string.
-* @s: input string.
-* Return: no return.
+* rev_string - reverses a string.
+* @s: input string.
+* Return: no return.
 */
-void rev_string(char *s)
+void rev_string(char *s)
 {
-int count = 0, i, j;
-char *str, temp;
-while (count >= 0)
+int count = 0, i, j;
+char *str, temp;
+while (count >= 0)
 {
-if (s[count] == '\0')
+if (s[count] == '\0')
 break;
 count++;
 }
-str = s;
-  for (i = 0; i < (count - 1); i++)
+str = s;
+for (i = 0; i < (count - 1); i++)
 {
-for (j = i + 1; j > 0; j--)
+for (j = i + 1; j > 0; j--)
 {
-temp = *(str + j);
-*(str + j) = *(str + (j - 1));
-*(str + (j - 1)) = temp;
+temp = *(str + j);
+*(str + j) = *(str + (j - 1));
+*(str + (j - 1)) = temp;
 }
 }
 }
