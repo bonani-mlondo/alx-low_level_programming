b590febb5402f1939f4c192eea916a7fe4443138
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-20 01:10:41 +0200

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/** * puts2 - prints one char out of 2 of a string.* @str: input string.
+/** 
+* puts2 - prints one char out of 2 of a string.* @str: input string.
 * Return: no return.
 */
 void puts2(char *str)
