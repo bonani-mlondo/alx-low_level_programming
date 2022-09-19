53686b4def2b8ce6ca5b08c798a303ab4207ac81
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-20 00:56:39 +0200

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,13 @@
-#include "main.h"
+#include "main.h"
 /**
 * _strlen - returns the length of a string.
-* @s: input string.
-* Return: length of a string.
+* @s: input string.
+* Return: length of a string.
 */
-int _strlen(char *s)
+int _strlen(char *s)
 {
-int count = 0;
-while (*(s + count) != '\0')
+int count = 0;
+while (*(s + count) != '\0')
 count++;
-return (count);
+return (count);
 }
