94bdec85d17bda516ee1af2caaccda707dd862e9
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-20 01:21:26 +0200

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,23 +1,23 @@
-#include "main.h"
+#include "main.h"
 /**
-* puts_half - prints half of a string.
-* @str: input string.
-* Return: no return.
+* puts_half - prints half of a string.
+* @str: input string.
+* Return: no return.
 */
-void puts_half(char *str)
+void puts_half(char *str)
 {
-int count = 0, i;
-while (count >= 0)
+int count = 0, i;
+while (count >= 0)
 {
-if (str[count] == '\0')
+if (str[count] == '\0')
 break;
 count++;
 }
-if (count % 2 == 1)
-i = count / 2;
+if (count % 2 == 1)
+i = count / 2;
 else
-i = (count - 1) / 2;
-for (i++; i < count; i++)
+i = (count - 1) / 2;
+for (i++; i < count; i++)
 _putchar(str[i]); 
 _putchar('\n');
 }
