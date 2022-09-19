4fc182bc521aa0d68a5b4aee7db0eb340ac14ad2
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-20 01:02:25 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,16 +4,16 @@
 * @s: input string.
 * Return: no return.
 */
-void print_rev(char *s)
+void print_rev(char *s)
 {
-int count = 0;
+int count = 0;
 while (count >= 0)
 {
-if (s[count] == '\0')
+if (s[count] == '\0')
 break;
 count++;
 }
-for (count--; count >= 0; count--)
+for (count--; count >= 0; count--)
 _putchar(s[count]);
 _putchar('\n');
 }
