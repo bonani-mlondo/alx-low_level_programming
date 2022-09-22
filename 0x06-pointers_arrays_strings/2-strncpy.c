e06475e0ad5460f40df509bf5e5b5ee62668f03b
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-22 18:49:11 +0200

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,21 @@
-#include "main.h"
+#include "main.h"
 /**
-* _strncpy - copies a string
-* @dest: destination to copy
-* @src: source to copy
-* @n: amount of places to copy
-* Return: value of dest
+* _strncpy - copies a string
+* @dest: destination to copy
+* @src: source to copy
+* @n: amount of places to copy
+* Return: value of dest
 */
-char *_strncpy(char *dest, char *src, int n) 
+char *_strncpy(char *dest, char *src, int n)
 {
-int i;
-for (i = 0; i < n && src[i] != '\0'; i++)
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
 {
-dest[i] = src[i];
+dest[i] = src[i];
 }
-for (; i < n; i++)
+for (; i < n; i++)
 {
-dest[i] = '\0';
+dest[i] = '\0';
 }
-return (dest);
+return (dest);
 }
