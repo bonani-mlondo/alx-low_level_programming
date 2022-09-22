c9e23f218beb14b5f439036ba70e8e5908d11b72
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-22 11:51:49 +0200

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,39 +1,39 @@
-#include "main.h"
-#include <stdio.h>
+#include "main.h"
+#include <stdio.h>
 /**
-* print_buffer - prints a buffer
-* @b: buffer.
-* @size: size of buffer.
-* Return: no return.
+* print_buffer - prints a buffer
+* @b: buffer.
+* @size: size of buffer.
+* Return: no return.
 */
-void print_buffer(char *b, int size)
+void print_buffer(char *b, int size)
 {
-int j, k, l;
-if (size <= 0)
+int j, k, l;
+if (size <= 0)
 printf("\n");
 else
 {
-for (j = 0; j < size; j += 10)
+for (j = 0; j < size; j += 10)
 {
-printf("%.8x:", j);
-for (k = j; k < j + 10; k++)
+printf("%.8x:", j);
+for (k = j; k < j + 10; k++)
 {
-if (k % 2 == 0)
-printf(" ");
-if (k < size)
-printf("%.2x", *(b + k));
+if (k % 2 == 0)
+printf(" ");
+if (k < size)
+printf("%.2x", *(b + k));
 else
 printf("  ");
 }
-printf(" ");
-for (l = j; l < j + 10; l++)
+printf(" ");
+for (l = j; l < j + 10; l++)
 {
-if (l >= size)
+if (l >= size)
 break;
-if (*(b + l) < 32 || *(b + l) > 126)
-printf("%c", '.');
+if (*(b + l) < 32 || *(b + l) > 126)
+printf("%c", '.');
 else
-printf("%c", *(b + l));
+printf("%c", *(b + l));
 }
 printf("\n");
 }
