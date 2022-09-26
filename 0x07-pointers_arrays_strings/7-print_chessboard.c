9230c7752e34a3d2965a8f6c5bdd01727bd249d4
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-27 00:32:17 +0200

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,20 @@
-#include "main.h"
+#include "main.h"
 /**
-* print_chessboard - prints the chessboard
-* @a: input pointer.
-* Return: no return.
+* print_chessboard - prints the chessboard
+* @a: input pointer.
+* Return: no return.
 */
-void print_chessboard(char (*a)[8])
+void print_chessboard(char (*a)[8])
 {
-unsigned int i, m = 0;
-for (i = 0; i < 64; i++)
+unsigned int i, m = 0;
+for (i = 0; i < 64; i++)
 {
-if (i % 8 == 0 && i != 0)
+if (i % 8 == 0 && i != 0)
 {
-m = i;
+m = i;
 _putchar('\n');
 }
-_putchar(a[i / 8][i - m]);
+_putchar(a[i / 8][i - m]);
 }
 _putchar('\n');
 }
