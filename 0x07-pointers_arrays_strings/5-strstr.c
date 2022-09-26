c501497f95cfd30723ec4ca78b0629325e25b09a
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-27 01:08:34 +0200

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,10 +9,8 @@ char *_strstr(char *haystack, char *needle)
 {
 int i;
 int j;
-  
 if (needle[0] == '\0')
 return (haystack);
-  
 for (i = 0; haystack[i] != '\0'; i++)
 {
 if (haystack[i] == needle[0])
@@ -25,7 +23,6 @@ break;
 if (needle[j] == '\0')
 return (haystack + i);
 }
-  
 }
 return ('\0');
- }
+}
