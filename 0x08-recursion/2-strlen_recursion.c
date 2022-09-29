0736813d6312d331c5571048cc1cf5658d802844
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-29 15:31:02 +0200

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,13 @@
-#include "main.h"
+#include "main.h"
 /**
-* _strlen_recursion - returns the length of a string.
-* @s: string
-* Return: the length of a string.
+* _strlen_recursion - returns the length of a string.
+* @s: string
+* Return: the length of a string.
 */
-int _strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
-if (*s == '\0')
-return (0);
+if (*s == '\0')
+return (0);
 else
-return (1 + _strlen_recursion(s + 1));
+return (1 + _strlen_recursion(s + 1));
 }
