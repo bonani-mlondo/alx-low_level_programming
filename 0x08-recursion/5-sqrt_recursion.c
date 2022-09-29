e84e4544edef7f138eb663381bbc70146e28831b
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-09-29 16:00:10 +0200

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -23,11 +23,11 @@ return (0 + power_operation(n, c + 1));
 */
 int _sqrt_recursion(int n)
 {
+if (n < 0)
+return (-1);
 if (n == 0)
 return (0);
-if (n < 0)
-return (0);
 if (n == 1)
-return (0);
+return (1);
 return (power_operation(n, 2));
 }
