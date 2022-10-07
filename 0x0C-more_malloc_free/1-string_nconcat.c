cebb5d052f795d9622cc3009078c31f1bf4aa86e
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-10-07 17:48:37 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 /**
 * string_nconcat - concatenates two strings.
-* @s1: first string.
-* @s2: second string.
-* @n: amount of bytes.
+* @s1: first string.
+* @s2: second string.
+* @n: amount of bytes.
 *
-* Return: pointer to the allocated memory.
-* if malloc fails, status value is equal to 98.
+* Return: pointer to the allocated memory.
+* if malloc fails, status value is equal to 98.
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
