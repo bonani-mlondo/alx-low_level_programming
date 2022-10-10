98d04abe992e739b685afc8298e7269d26ae9b4f
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-10-11 01:57:01 +0200

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,14 +1,14 @@
-#include "dog.h"
-#include <stdlib.h>
+#include "dog.h"
+#include <stdlib.h>
 /**
-* free_dog - frees dogs.
-* @d: struct dog.
+* free_dog - frees dogs.
+* @d: struct dog.
 *
-* Return: no return.
+* Return: no return.
 */
-void free_dog(dog_t *d)
+void free_dog(dog_t *d)
 {
-if (d)
+if (d)
 {
 free(d->name);
 free(d->owner);
