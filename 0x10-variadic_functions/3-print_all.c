8e9289e5f24588a0d96e81782e51cb637cc90d55
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-10-15 02:38:09 +0200

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,49 @@
-#include "variadic_functions.h"
+#include "variadic_functions.h"
 /**
-* print_all - prints anything.
-* @format: a list of types of arguments passed to the function.
+* print_all - prints anything.
+* @format: a list of types of arguments passed to the function.
 *
-* Return: no return.
+* Return: no return.
 */
-void print_all(const char * const format, ...)
+void print_all(const char * const format, ...)
 {
-va_list valist;
-unsigned int i = 0, j, c = 0;
-char *str;
-const char t_arg[] = "cifs";
-va_start(valist, format);
-while (format && format[i])
+va_list valist;
+unsigned int i = 0, j, c = 0;
+char *str;
+const char t_arg[] = "cifs";
+va_start(valist, format);
+while (format && format[i])
 {
-j = 0;
-while (t_arg[j])
+j = 0;
+while (t_arg[j])
 {
-if (format[i] == t_arg[j] && c)
+if (format[i] == t_arg[j] && c)
 {
-printf(", ");
+printf(", ");
 break;
-} j++;
+} j++;
 }
-switch (format[i])
+switch (format[i])
 {
-case 'c':
-printf("%c", va_arg(valist, int)), c = 1;
+case 'c':
+printf("%c", va_arg(valist, int)), c = 1;
 break;
-case 'i':
-printf("%d", va_arg(valist, int)), c = 1;
+case 'i':
+printf("%d", va_arg(valist, int)), c = 1;
 break;
-case 'f':
-printf("%f", va_arg(valist, double)), c = 1;
+case 'f':
+printf("%f", va_arg(valist, double)), c = 1;
 break;
-case 's':
-str = va_arg(valist, char *), c = 1;
-if (!str)
+case 's':
+str = va_arg(valist, char *), c = 1;
+if (!str)
 {
 printf("(nil)");
 break;
 }
-printf("%s", str);
+printf("%s", str);
 break;
-} i++;
+} i++;
 }
-printf("\n"), va_end(valist);
+printf("\n"), va_end(valist);
 }
