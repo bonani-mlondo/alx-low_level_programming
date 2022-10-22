e4dd461be9567dd9c46213a11eff2fc4fe2c5bf6
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-10-22 04:08:42 +0200

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,15 +1,15 @@
-#include "lists.h"
+#include "lists.h"
 /**
-* free_list - frees a list
-* @head: head of the linked list.
-* Return: no return.
+* free_list - frees a list
+* @head: head of the linked list.
+* Return: no return.
 */
-void free_list(list_t *head)
+void free_list(list_t *head)
 {
-list_t *current_node;
-while ((current_node = head) != NULL)
+list_t *current_node;
+while ((current_node = head) != NULL)
 {
-head = head->next;
+head = head->next;
 free(current_node->str);
 free(current_node);
 }
