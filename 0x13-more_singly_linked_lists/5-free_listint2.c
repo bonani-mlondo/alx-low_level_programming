29e7064432570be24873b80de3a20357bd54dde6
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-10-25 18:40:29 +0200

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,19 +1,19 @@
-#include "lists.h"
+#include "lists.h"
 /**
-* free_listint2 - free the linked list
-* @head : pointer to list
-* Return: void
+* free_listint2 - free the linked list
+* @head : pointer to list
+* Return: void
 */
-void free_listint2(listint_t **head)
+void free_listint2(listint_t **head)
 {
-listint_t *temp;
-if (head == NULL)
+listint_t *temp;
+if (head == NULL)
 return;
-while (*head)
+while (*head)
 {
-temp = *head;
-*head = (*head)->next;
+temp = *head;
+*head = (*head)->next;
 free(temp);
 }
-head = NULL;
+head = NULL;
 }
