92dbb59768efddb9eec944cc3edc346a85f8cc48
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-10-28 20:20:33 +0200

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,18 @@
-#include "main.h"
+#include "main.h"
 /**
-* print_binary - prints the binary representation
-* of a number.
-* @n: unsigned long int.
+* print_binary - prints the binary representation
+* of a number.
+* @n: unsigned long int.
 *
-* Return: no return.
+* Return: no return.
 */
-void print_binary(unsigned long int n)
+void print_binary(unsigned long int n)
 {
-if (n >> 0)
+if (n >> 0)
 {
-if (n >> 1)
-print_binary(n >> 1);
-_putchar((n & 1) + '0');
+if (n >> 1)
+print_binary(n >> 1);
+_putchar((n & 1) + '0');
 }
 else
 {
