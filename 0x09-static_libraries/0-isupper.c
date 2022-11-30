e6ac3a6698f7e5f457ff074e4f9ed6e6c5a9ed66
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-12-01 01:34:14 +0200

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,17 +1,17 @@
-#include "main.h"
+#include "main.h"
 /**
-* _isupper - checkes upper case
-* @c: print int
-* Return: give 1 or 0
+* _isupper - checkes upper case
+* @c: print int
+* Return: give 1 or 0
 */
-int _isupper(int c)
+int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
+if (c >= 'A' && c <= 'Z')
 {
-return (1);
+return (1);
 }
 else
 {
-return (0);
+return (0);
 }
 }
