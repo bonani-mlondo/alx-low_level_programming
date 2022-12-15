5262fa1a1d6d5d062fb4cbb0fb0d4865c7946b32
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-12-16 00:38:18 +0200

    Update 0-strcat.c

--- 0x18-dynamic_libraries/0-strcat.c
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -1,20 +1,20 @@
-#include "main.h"
+#include "main.h"
 /**
-* _strcat - concatenate two strings
-* @dest: char string to concatenate to
-* @src: char string
-* Return: pointer to resulting string `dest`
+* _strcat - concatenate two strings
+* @dest: char string to concatenate to
+* @src: char string
+* Return: pointer to resulting string `dest`
 */
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-int i, c;
-for (i = 0; dest[i] != '\0'; i++)
+int i, c;
+for (i = 0; dest[i] != '\0'; i++)
 ;
-for (c = 0; src[c] != '\0'; c++)
+for (c = 0; src[c] != '\0'; c++)
 {
-dest[i] = src[c];
+dest[i] = src[c];
 i++;
 }
-dest[i] = '\0';
-return (dest);
+dest[i] = '\0';
+return (dest);
 }
