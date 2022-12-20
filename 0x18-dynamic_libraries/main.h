eac6c0c4790cf2fa1cd205d030b362245c126f2b
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-12-20 16:47:18 +0200

    Update main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -27,4 +29,5 @@ int sub(int a, int b);
 int mul(int a, int b);
 int mod(int a, int b);
 extern div_t div(int __numer, int __denom);
+
 #endif /* MAIN_H */
